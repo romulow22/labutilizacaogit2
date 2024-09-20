1904c00b20eb88186a1116e4e17baaf288239375
Romulo Alves <romuloass@hotmail.com>
2024-09-19 23:25:16 -0300

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,11 @@
-import { blackFridayFlag } from '@/app/flags';
+import Image from "next/image";
 
-export default async function Page() {
-  const black_friday = await blackFridayFlag();
-
+export default function Home() {
   return (
-      <div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1> Welcome to the ConchaYOro App - deploy v2</h1>
-        { black_friday ? <button> Promo </button> : <button> Normal </button> }
       </div>
+    </main>
   )
-};
+}
